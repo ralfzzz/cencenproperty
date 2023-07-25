970433d87056ecf93a8623cd36913558af008df4
ralfzzz <azbimanyu@gmail.com>
2023-07-25 20:04:09 +0700

    updater
    create home view, route, and styles.css

---
+++ cencenproperty-app/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- CSS STYLES --}}
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Cencen Property</title>
+</head>
+<body>
+    <div class="home">HOME CENCEN PROPERTY</div>
+</body>
+</html>
