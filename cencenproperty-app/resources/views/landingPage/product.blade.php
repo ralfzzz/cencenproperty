e89ee4eaf517958d685658e207ed04d150f0c9ad
Haidaravi <haidaraviardi01@gmail.com>
2023-09-23 21:45:32 +0700

    Update product.blade.php

--- cencenproperty-app/resources/views/landingPage/product.blade.php
+++ cencenproperty-app/resources/views/landingPage/product.blade.php
@@ -11,15 +11,97 @@
   </head>
   <body>
     <!-- NavBar -->
-    <nav class="navbar navbar-expand-xxxl navbar-light overflow-hidden shadow mb-5">
-        <div class="container-fluid justify-content-space-around">
+    <div class="shadow container-fluid">
+    <nav class="navbar navbar-expand-xxxl navbar-light overflow-hidden" >
+        <div class="container-fluid" style="justify-content: flex-end;">
             <a class="navbar-brand" href="#">
                 <!-- Brand Logo -->
                 <div id="crop">
                     <img src="/img/live_logo.png" alt="Live@" id="logo">
                 </div>
             </a>
-            <button class="btn btn-primary ms-auto buttongold" id="btn-titip">Titip&Jual</button>
+            <button class="btn btn-primary ms-auto buttongold" id="btn-titip">Titip Jual</button>
+    
+    <style type="text/css">
+      a img {
+        width: 46%;
+        margin: 2px;
+        margin-right: 0px;
+        align-items: center;
+      }
+        </style>
+    </head>
+    <body>
+        <!-- Button trigger modal -->
+        <button
+            type="button"
+            class="btn p-2 "
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+            style="background-color: transparent"
+            ;
+        >
+            <i
+                class="fa-brands fa-instagram fa-2xl"
+                style="color: #bc9c22"
+            ></i>
+        </button>
+
+        <!-- Modal -->
+        <div
+            class="modal fade"
+            id="staticBackdrop"
+            data-bs-backdrop="static"
+            data-bs-keyboard="false"
+            tabindex="-1"
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true"
+        >
+            <div
+                class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+            >
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                            <i
+                                class="fa-brands fa-instagram fa-bounce fa-lg"
+                            ></i>
+                            Cencen Property
+                        </h1>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body" id="instafeed-container"></div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn"
+                            data-bs-dismiss="modal"
+                            style="background-color: #bc9c22; color: white"
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/gh/stevenschobert/instafeed.js@2.0.0rc1/src/instafeed.min.js"></script>
+        <script type="text/javascript">
+            var userFeed = new Instafeed({
+                get: "user",
+                target: "instafeed-container",
+                resolution: "low_resolution",
+                accessToken:
+                    "IGQWRPTk8xNUJXSjdQa3oxQmlXRjJLMnBqNDFkSDd0WFdlNVRNVUswQWNhOFJjUFdIdWFRTG1EOHNTSW15VTg5R2N4aXg5WHZAKMVFLRVNmNTh0Yl9sb3JMcFZADOUVLVC1IUzIybDZA6ZATVyaEVXOW84OE9GcDhFSmcZD",
+            });
+            userFeed.run();
+        </script>
+
             <button 
             class="navbar-toggler" 
             type="button" 
@@ -36,119 +118,137 @@
                 <ul class="navbar-nav ms-auto text-left">
                     <li>
                         <a href="#" class="nav-link">
-                            Testimoni
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link">
-                            About
+                            Home
                         </a>
                     </li>
                 </ul>
             </div>  
-        </div>   
+        </div>
+       
     </nav>
-  	<!-- Column -->
-  	<div class="container text-center">
-	  <div class="row align-items-start">
-	    <div class="col-md-7">
-	    	<p class="container h1 text-start fw-bold">Cencen Property <span class="badge fw-normal fs-6 px-3 rounded-1" style="background-color: #BC9C22;">Dijual</span> <span class="badge fw-normal fs-6 px-3 rounded-1" style="background-color: #BC9C22;">Rumah</span></p>
-	    	<img src="https://source.unsplash.com/home" class="container rounded-2 my-4 object-fit-cover" style="max-height: 350px;">
-	    	
-			<div class="container mb-5">
-			  <!-- Nav tabs -->
-			  <ul class="nav nav-underline" role="tablist">
-			    <li class="nav-item">
-			      <a class="nav-link active" data-bs-toggle="tab" href="#home" style="color: #BC9C22;">Deskripsi</a>
-			    </li>
-			    <li class="nav-item">
-			      <a class="nav-link" data-bs-toggle="tab" href="#menu1" style="color: #BC9C22;">Fasilitas</a>
-			    </li>
-			    <li class="nav-item">
-			      <a class="nav-link" data-bs-toggle="tab" href="#menu2" style="color: #BC9C22;">Lokasi</a>
-			    </li>
-			  </ul>
+    </div>
 
-			  <!-- Tab panes -->
-			  <div class="tab-content text-start">
-			    <div id="home" class="container tab-pane active"><br>
-			    	<p style="color: #BC9C22">Harga</p>
-			    	<p>Rp. 500 jt</p>
-			    	<hr>
-			    	<div class="row"> 
-			    		<div class="col-md-3">
-			    			<p style="color: #BC9C22">Kamar Tidur</p>
-			    			<a><span class="fa fa-bed"></span> 2</a>
-			    		</div>
-			    		<div class="col-md-3">
-			    			<p style="color: #BC9C22">Kamar Mandi</p>
-			    			<a><span class="fa fa-bath"></span> 1</a>
-			    		</div>
-			    	</div>
-			    	<hr>
-			    	<div class="row"> 
-			    		<div class="col-md-3">
-			    			<p style="color: #BC9C22">Tipe</p>
-			    			<p>Land Size</p>
-			    		</div>
-			    		<div class="col-md-3">
-			    			<p style="color: #BC9C22">Luas</p>
-			    			<p>150 sqm</p>
-			    		</div>
-			    	</div>
-			    	<hr>
-			    	<p style="color: #BC9C22">Tentang</p>
-			    	<p class="text-align-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-			      
-			    </div>
+		<!-- Column -->
+		<div class="container text-center">
+		<div class="row align-items-start">
+			<div class="col-md-7">
+				<p class="container h1 text-start fw-bold">Cencen Property <span class="badge fw-normal fs-6 px-3 rounded-1" style="background-color: #BC9C22;">Dijual</span> <span class="badge fw-normal fs-6 px-3 rounded-1" style="background-color: #BC9C22;">Rumah</span></p>
+				<div id="carouselExample" class="carousel slide my-3">
+					<div class="carousel-inner rounded-3"  style="max-height: 300px;">
+						<div class="carousel-item active">
+							<img src="sss.jpg" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img src="aaa.jpg" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img src="types-of-homes-hero.jpg" class="d-block w-100" alt="...">
+						</div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Previous</span>
+					</button>
+					<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Next</span>
+					</button>
+				</div>
+				
+			<div class="container">
+				<!-- Nav tabs -->
+				<ul class="nav nav-underline" role="tablist">
+					<li class="nav-item">
+						<a class="nav-link active" data-bs-toggle="tab" href="#home" style="color: #BC9C22;">Deskripsi</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" data-bs-toggle="tab" href="#menu1" style="color: #BC9C22;">Fasilitas</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" data-bs-toggle="tab" href="#menu2" style="color: #BC9C22;">Lokasi</a>
+					</li>
+				</ul>
 
-			    <div id="menu1" class="container tab-pane fade"><br>
-			      	<a><span class="bi bi-water"></span> Kolam renang</a><br>
-			      	<a><span class="bi bi-house-door-fill"></span> Clubhouse</a><br>
-			      	<a><span class="bi bi-wifi"></span> Wifi</a>
-			    </div>
-			    <div id="menu2" class="container tab-pane fade"><br>
-			    	<div class="row align-items-start">
-			      <div class="col-md-5">
+				<!-- Tab panes -->
+				<div class="tab-content text-start">
+					<div id="home" class="container tab-pane active"><br>
+						<p style="color: #BC9C22">Harga</p>
+						<p>Rp. 500 jt</p>
+						<hr>
+						<div class="row"> 
+							<div class="col-md-3">
+								<p style="color: #BC9C22">Kamar Tidur</p>
+								<a><span class="fa fa-bed"></span> 2</a>
+							</div>
+							<div class="col-md-3">
+								<p style="color: #BC9C22">Kamar Mandi</p>
+								<a><span class="fa fa-bath"></span> 1</a>
+							</div>
+						</div>
+						<hr>
+						<div class="row"> 
+							<div class="col-md-3">
+								<p style="color: #BC9C22">Tipe</p>
+								<p>Land Size</p>
+							</div>
+							<div class="col-md-3">
+								<p style="color: #BC9C22">Luas</p>
+								<p>150 sqm</p>
+							</div>
+						</div>
+						<hr>
+						<p style="color: #BC9C22">Tentang</p>
+						<p class="text-align-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+						
+					</div>
+
+					<div id="menu1" class="container tab-pane fade"><br>
+							<li>Kolam renang</li>
+							<li>Clubhouse</li>
+							<li>Taman</li>
+					</div>
+					<div id="menu2" class="container tab-pane fade"><br>
+						<div class="row align-items-start">
+						<div class="col-md-5">
 							<p style="color: #BC9C22">Alamat</p>
 							<p>Jl. Gatot subroto</p>
 						</div>
 
 						<div class="col-md-5">
 							<div id="map-container-google-1" class="container">
-							  <iframe src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
-							    style="border:0" class="w-100" allowfullscreen class="mx-0"></iframe>
+								<iframe src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+									style="border:0" class="w-100" allowfullscreen class="mx-0"></iframe>
 							</div>
 						</div>
 						</div>
 
-			    </div>
-			  </div>
+					</div>
+				</div>
 			</div>
 
 		<!-- Tab panes -->
 		<div class="tab-content">
-		  <div class="tab-pane container active" id="home"></div>
-		  <div class="tab-pane container fade" id="menu1"></div>
-		  <div class="tab-pane container fade" id="menu2"></div>
+			<div class="tab-pane container active" id="home"></div>
+			<div class="tab-pane container fade" id="menu1"></div>
+			<div class="tab-pane container fade" id="menu2"></div>
 		</div>
 		</div>
-    <div class="col-md-5 sticky-top container-md">
-      <div class="card mt-3">
-      	<img class="card-img-top mx-5 mt-2 mx-auto d-block" src="https://source.unsplash.com/200x200?home" alt="Card image" style="width:250px">
-			  <div class="card-body">
-			  	<div class="row d-flex justify-content-center">
-			  		<div class="col-md-5 d-grid">
-			  			<button type="button" class="btn text-white mb-2" style="background-color: #BC9C22;"> <i class="fa fa-whatsapp" aria-hidden="true"></i> Whatsapp</button>
-			  		</div>
-			  		<div class="col-md-5 d-grid">
-			  			<button type="button" class="btn text-white mb-2" style="background-color: #BC9C22;"><i class="fa fa-phone" aria-hidden="true"></i> Phone</button>
-			  		</div>
-			  	</div>
-			  </div>
+		<div class="col-md-5 sticky-top container-md">
+			<div class="card mt-3">
+				<img class="card-img-top mx-5 mt-2 mx-auto d-block" src="Karya_Seni_Tanpa_Judul 3_copy.png" alt="Card image" style="width:250px">
+				<div class="card-body">
+					<div class="row d-flex justify-content-center">
+						<div class="col-md-5 d-grid">
+							<button type="button" class="btn text-white mb-2" style="background-color: #BC9C22;"> <i class="fa fa-whatsapp" aria-hidden="true"></i> Whatsapp</button>
+						</div>
+						<div class="col-md-5 d-grid">
+							<button type="button" class="btn text-white mb-2" style="background-color: #BC9C22;"><i class="fa fa-phone" aria-hidden="true"></i> Phone</button>
+						</div>
+					</div>
+				</div>
 			</div>
-    </div>
-	  </div>
+		</div>
+		</div>
 	</div>
 
 <!-- Footer -->
