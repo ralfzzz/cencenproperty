2c512c1b81836936ec40cdd786ea34942e50d4e9
Haidaravi <haidaraviardi01@gmail.com>
2023-10-20 21:39:53 +0700

    Update main.blade.php

--- cencenproperty-app/resources/views/landingPage/layouts/main.blade.php
+++ cencenproperty-app/resources/views/landingPage/layouts/main.blade.php
@@ -29,6 +29,7 @@
       referrerpolicy="no-referrer"
     ></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style type="text/css">
         a img {
           width: 46%;
