44dd16613ab2e7684eb374e210eced31195f3176
Haidaravi <haidaraviardi01@gmail.com>
2023-10-20 22:23:34 +0700

    a

--- cencenproperty-app/resources/views/landingPage/partials/floatbutton.blade.php
+++ cencenproperty-app/resources/views/landingPage/partials/floatbutton.blade.php
@@ -3,7 +3,7 @@
 <div id="floatIcon" class="dropup-center float-end sticky-bottom p-5" >
   <button type="button" class="btn btn-lg hide-toggle rounded-circle text-white" data-bs-toggle="dropdown" aria-expanded="false" aria-haspopup="true"style="background-color: #BC9C22"><i class="bi bi-chat"></i>
   </button>
-  <ul class="dropdown-menu">
+  <ul id="dropdownnew" class="dropdown-menu">
     <li>
     	<a class="dropdown-item d-flex justify-content-center" href="#"><i class="fa fa-whatsapp fs-1" aria-hidden="true"></i></a>
     </li>  
